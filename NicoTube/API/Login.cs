8e273a171afd0704ac4ca84ee6a441f82a877580
kinketsucom <sekibotbot@gmail.com>
2018-06-01 17:56:08 +0900

    1st

---
+++ NicoTube/API/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace NicoTube.API
+{
+    class Login
+    {
+        /// ログイン時のセッションID
+        public static CookieContainer LoginCookie = new CookieContainer();
+
+        public static void LoginToNicovideo(string mail, string passwd)
+        {
+            var req = (HttpWebRequest)HttpWebRequest.Create("https://secure.nicovideo.jp/secure/login?site=niconico");
+            req.Method = "POST";
+
+            var args = new Dictionary<string, string>
+                           {
+                               {"mail", mail},
+                               {"password", passwd}
+                           };
+
+            var s = args.Select(x => x.Key + "=" + x.Value).Aggregate((x, y) => x + "&" + y);
+            var bs = Encoding.UTF8.GetBytes(s);
+            req.ContentLength = bs.Length;
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.CookieContainer = LoginCookie;
+            req.GetRequestStream().Write(bs, 0, bs.Length);
+
+            string result;
+            try
+            {
+                var resp = (HttpWebResponse)req.GetResponse();
+                result = new StreamReader(resp.GetResponseStream()).ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error during login proccess.", ex);
+            }
+            if (result.Contains("wrongPass"))
+            {
+                throw new Exception("Wrong ID or Password. Login Failed.");
+            }
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
---
+++ NicoTube/API/NicoVideo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoTube.API
+{
+    class NicoVideo
+    {
+        /// <summary>
+        /// 指定された動画を視聴できるクッキーをセットされたWebClientを取得する.
+        /// </summary>
+        /// <param name="id">動画ID</param>
+        /// <returns>クッキーを使用するCustomWebClientk</returns>
+        public static CustomWebClient GetWebClientForFlv(string id)
+        {
+            var client = new CustomWebClient();
+            client.CookieContainer.Add(GetWatchCookie(id));
+
+            return client;
+        }
+    }
+}
